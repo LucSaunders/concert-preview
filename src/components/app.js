0d67b933bd3bfb69afd8296fb1f37503345f3452
Jason Doss <doss2k@gmail.com>
2019-03-28 15:29:55 -0400

    Add event list view functionality

--- src/components/app.js
+++ src/components/app.js
@@ -12,9 +12,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        
         <EventListShow />
-        <VideoListShow />
+        
       </div>
     );
   }
---
+++ src/components/event-list.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Link } from "react-router-dom";
+import { fetchEvents } from "../actions";
+
+class EventListShow extends Component {
+  componentDidMount() {
+    this.props.fetchEvents('Raleigh');
+  }
+
+  renderEvents() {    
+    const eventsArray = this.props.events;
+    return eventsArray.map(event => {
+      return (
+        <li style={{marginBottom: 10}} className="event-list-item" key={event.id}>          
+          <div>{event.title} - {event.city_name}, {event.region_name}</div>          
+          <div>{event.venue_name} - {event.start_time}</div>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    if(!this.props.events.length) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <div>
+        <h3>Events</h3>
+        <ul className="event-list-group">
+          {this.renderEvents()}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { events: state.events };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchEvents }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventListShow);
+
