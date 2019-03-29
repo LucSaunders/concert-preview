93cdef2ba6225602447e04ad464550d2c8f50d1f
Jason Doss <doss2k@gmail.com>
2019-03-29 10:13:19 -0400

    fix event reducer return

--- src/reducers/reducer-events.js
+++ src/reducers/reducer-events.js
@@ -5,7 +5,7 @@ import { FETCH_EVENTS } from "../actions";
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_EVENTS:
-    return [...action.payload];
+      return [...action.payload.data.events.event];
     default:
       return state;
   }
