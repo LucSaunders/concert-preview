f9bbbe3ef956b1c928c11b3032f6bb94e8e44d8b
Jason Doss <doss2k@gmail.com>
2019-03-29 11:40:26 -0400

    Add error checking when searching for cities and get no results

--- src/reducers/reducer-events.js
+++ src/reducers/reducer-events.js
@@ -1,11 +1,15 @@
 import { FETCH_EVENTS } from "../actions";
 
-/* Reducer for the event list */
+// Reducer for the event list. Checks to verify you got results back from your search
 
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_EVENTS:
-      return [...action.payload.data.events.event];
+      if(action.payload.data.events) {
+        return [...action.payload.data.events.event];
+      } else {
+        return ["NoResults"];
+      }
     default:
       return state;
   }
