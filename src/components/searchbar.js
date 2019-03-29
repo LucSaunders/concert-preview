4b6f8622b734aebbe9359deea27fe247181c9cc2
Carson Poe <cgp6012@gmail.com>
2019-03-29 11:58:19 -0400

    update searchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchEvents } from '../actions';
-import 'font-awesome/css/font-awesome.css'
+
 
 class SearchBar extends Component {
  constructor(props) {
